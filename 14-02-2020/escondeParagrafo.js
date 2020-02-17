7c5a9cb39cd3ead365fc40d3f91839efd91ca02c
Davi Carvalho <davicarv13@gmail.com>
2020-02-17 09:11:11 -0300

    Atualiza escondeParagrafo

--- 14-02-2020/escondeParagrafo.js
+++ 14-02-2020/escondeParagrafo.js
@@ -1,35 +1,54 @@
+var teste1 = true, teste2 = true, teste3 = true;
+
 function escondeParagrafo1(){
 	var paragrafo1 = document.getElementById("conteudo_1");
 	var link1 = document.getElementById("enlace_1");
+	if(teste1 == true){
+		paragrafo1.style.display = "none";
+		link1.innerHTML = "Mostrar Conteudo";
+		teste1 = false;
+	}
+	else{
+		paragrafo1.style.display = "block";
+		link1.innerHTML = "Ocultar Conteudo";
+		teste1 = true;
+	}
 
-	paragrafo1.style.display = "none";
-	link1.innerHTML = "Mostrar Conteudo";
 }
 
 function escondeParagrafo2(){
 	var paragrafo2 = document.getElementById("conteudo_2");
 	var link2 = document.getElementById("enlace_2");
-
-	paragrafo2.style.display = "none";
-	link2.innerHTML = "Mostrar Conteudo";
+	if(teste2 == true){
+		paragrafo2.style.display = "none";
+		link2.innerHTML = "Mostrar Conteudo";
+		teste2 = false;
+	}
+	else{
+		paragrafo2.style.display = "block";
+		link2.innerHTML = "Ocultar Conteudo";
+		teste2 = true;
+	}
 }
 
 function escondeParagrafo3(){
 	var paragrafo3 = document.getElementById("conteudo_3");
 	var link3 = document.getElementById("enlace_3");
-
-	paragrafo3.style.display = "none";
-	link3.innerHTML = "Mostrar Conteudo";
-}
-
-function stopDefAction(evt) {
-    evt.preventDefault();
+	if(teste3 == true){
+		paragrafo3.style.display = "none";
+		link3.innerHTML = "Mostrar Conteudo";
+		teste3 = false;
+	}
+	else{
+		paragrafo3.style.display = "block";
+		link3.innerHTML = "Ocultar Conteudo";
+		teste3 = true;
+	}
 }
 
 function carregaFuncoes(){
 	var enlace_1 = document.getElementById("enlace_1");
 	enlace_1.addEventListener("click", escondeParagrafo1, false);	
-	enlace_1.addEventListener("click", stopDefAction, false);	
 
 	var enlace_2 = document.getElementById("enlace_2");
 	enlace_2.addEventListener("click", escondeParagrafo2, false);	
@@ -39,6 +58,4 @@ function carregaFuncoes(){
 }
 
 var html = document.getElementsByTagName("html")[0];
-html.addEventListener("load", carregaFuncoes, false);
-
-carregaFuncoes();
+html.addEventListener("load", carregaFuncoes(), false);
